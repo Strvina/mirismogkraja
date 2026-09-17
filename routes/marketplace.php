d061a58a172c3bafe5ac2f722d5e04c325d1da10
Vladimir <vladanicic02@gmail.com>
2026-09-17 19:45:11 +0200

    3.7 Public product list with filters and sorting
    Filters: category_id, city, min_price/max_price. Sort: latest (default), price_asc, price_desc. Only active products from active households.
    
    81 tests pass, Pint clean.

--- routes/marketplace.php
+++ routes/marketplace.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/domacinstva', [HouseholdController::class, 'index'])->name('marketplace.households.index');
 Route::get('/domacinstvo/{household:slug}', [HouseholdController::class, 'show'])->name('marketplace.households.show');
+Route::get('/proizvodi', [ProductController::class, 'index'])->name('marketplace.products.index');
 Route::get('/proizvod/{product:slug}', [ProductController::class, 'show'])->name('marketplace.products.show');
